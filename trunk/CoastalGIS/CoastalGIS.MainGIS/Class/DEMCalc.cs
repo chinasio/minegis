a2baa6e02eff17b8a97a47119d8eda5b1c08bf10
MineEnvGIS@gmail.com <MineEnvGIS@gmail.com@490b2540-26a2-b16c-8ac8-b3a2116dd9a4>
2011-11-04 02:53:34 +0000

    封装DEM计算坡度坡向方法，封装方法位于MainGIS\Class\DEMCalc.CS

---
+++ trunk/CoastalGIS/CoastalGIS.MainGIS/Class/DEMCalc.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ESRI.ArcGIS.Controls;
+using ESRI.ArcGIS.Carto;
+using System.Windows.Forms;
+using System.IO;
+using ESRI.ArcGIS.Geodatabase;
+using ESRI.ArcGIS.GeoAnalyst;
+using ESRI.ArcGIS.DataSourcesRaster;
+
+namespace CoastalGIS.MainGIS.Class
+{
+    public class DEMCalc
+    {
+        public DEMCalc(IMapControlDefault mapControl)
+        {
+            m_mapControl = mapControl;
+        }
+
+        private IMapControlDefault m_mapControl;
+
+        /// <summary>
+        /// 计算DEM坡度
+        /// </summary>
+        /// <param name="rasterLayer">输入DEM图层</param>
+        /// <param name="outputFullPath">输出坡度图路径，类型为IMAGE，文件名为img</param>
+        public void CreateRasterSlope(ILayer rasterLayer, string outputFullPath)
+        {
+            if (File.Exists(outputFullPath))
+            {
+                MessageBox.Show(outputFullPath + "已经存在，请重命名");
+                return;
+            }
+            IRasterLayer rasterLyr = rasterLayer as IRasterLayer;
+            IRaster raster = rasterLyr.Raster;
+            RasterSurfaceOpClass rasterOpCls = new RasterSurfaceOpClass();
+            object o = 1;
+            IGeoDataset geoDataset = raster as IGeoDataset;
+            esriGeoAnalysisSlopeEnum geoType = esriGeoAnalysisSlopeEnum.esriGeoAnalysisSlopeDegrees;
+            IGeoDataset outGeodataset = rasterOpCls.Slope(geoDataset, geoType, ref o);
+            IRaster pRaster = outGeodataset as IRaster;
+
+            FileInfo fi = new FileInfo(outputFullPath);
+            string fileDir = fi.Directory.FullName;
+            string name = fi.Name;
+            if (!Directory.Exists(fileDir))
+                Directory.CreateDirectory(fileDir);
+            IWorkspaceFactory pWSF = new RasterWorkspaceFactoryClass();
+            IWorkspace pWS = pWSF.OpenFromFile(fileDir, 0);
+            ISaveAs pSaveAs = pRaster as ISaveAs;
+            pSaveAs.SaveAs(name, pWS, "IMAGINE Image");
+            IRasterLayer rasterLyr2 = new RasterLayerClass();
+            rasterLyr2.CreateFromRaster(pRaster);
+            rasterLyr2.Name = name;
+            m_mapControl.Map.AddLayer(rasterLyr2 as ILayer);
+        }
+
+        /// <summary>
+        /// 计算DEM坡向
+        /// </summary>
+        /// <param name="rasterLayer">输入DEM图层</param>
+        /// <param name="outputFullPath">输出坡度图路径，类型为IMAGE，文件名为img</param>
+        public void CreateRasterAspect(ILayer rasterLayer, string outputFullPath)
+        {
+            if (File.Exists(outputFullPath))
+            {
+                MessageBox.Show(outputFullPath + "已经存在，请重命名");
+                return;
+            }
+            IRasterLayer rasterLyr = rasterLayer as IRasterLayer;
+            IRaster raster = rasterLyr.Raster;
+            RasterSurfaceOpClass rasterOpCls = new RasterSurfaceOpClass();
+            IGeoDataset geoDataset = raster as IGeoDataset;
+            IGeoDataset outGeodataset = rasterOpCls.Aspect(geoDataset);
+            IRaster pRaster = outGeodataset as IRaster;
+
+            FileInfo fi = new FileInfo(outputFullPath);
+            string fileDir = fi.Directory.FullName;
+            string name = fi.Name;
+            if (!Directory.Exists(fileDir))
+                Directory.CreateDirectory(fileDir);
+            IWorkspaceFactory pWSF = new RasterWorkspaceFactoryClass();
+            IWorkspace pWS = pWSF.OpenFromFile(fileDir, 0);
+            ISaveAs pSaveAs = pRaster as ISaveAs;
+            pSaveAs.SaveAs(name, pWS, "IMAGINE Image");
+            IRasterLayer rasterLyr2 = new RasterLayerClass();
+            rasterLyr2.CreateFromRaster(pRaster);
+            rasterLyr2.Name = name;
+            m_mapControl.Map.AddLayer(rasterLyr2 as ILayer);
+        }
+    }
+}
